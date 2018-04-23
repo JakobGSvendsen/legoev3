b22ba77dc298398a0983048698002e8a7adec210
Jakob Gottlieb Svendsen <jgs@coretech.dk>
2018-04-23 11:42:52 +0200

    Added .Net Core Library

---
+++ Lego.Ev3.Tester.NetCoreConsole/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading.Tasks;
+using Lego.Ev3.Core;
+using Lego.EV3.NetCore;
+
+namespace Lego.Ev3.Tester.NetCoreConsole
+{
+    class Program
+    {
+
+        static Brick _brick;
+        static void Main(string[] args)
+        {
+            Task t = Test();
+            t.Wait();
+            System.Console.ReadKey();
+        }
+
+        static async Task Test()
+        {
+            //_brick = new Brick(new UsbCommunication());
+            //_brick = new Brick(new BluetoothCommunication("COM5"));
+            _brick = new Brick(new NetworkCommunication("192.168.2.8"));
+
+            _brick.BrickChanged += _brick_BrickChanged;
+
+            System.Console.WriteLine("Connecting...");
+            await _brick.ConnectAsync();
+
+            System.Console.WriteLine("Connected...Turning motor...");
+            await _brick.DirectCommand.TurnMotorAtSpeedForTimeAsync(OutputPort.A, 0x50, 1000, false);
+
+            System.Console.WriteLine("Motor turned...beeping...");
+            await _brick.DirectCommand.PlayToneAsync(0x50, 5000, 500);
+
+            System.Console.WriteLine("Beeped...done!");
+        }
+
+        static void _brick_BrickChanged(object sender, BrickChangedEventArgs e)
+        {
+            System.Console.WriteLine(e.Ports[InputPort.One].SIValue);
+        }
+    }
+}
